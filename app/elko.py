8b6a6e4fa7d1f70251ddbcd8c693e8762be9d799
tav <tav@espians.com>
2018-01-09 13:47:17 +0000

    app: use the get.elko.io subdomain

--- app/elko.py
+++ app/elko.py
@@ -8,13 +8,13 @@ import release
 from weblite import app, handle, read
 
 INSTALL_SCRIPT = read('install.sh') % {
-    'darwin': release.DARWIN,
-    'linux': release.LINUX,
+    'darwin': release.darwin,
+    'linux': release.linux,
 }
 
 @handle('/')
 def root(ctx):
-    if ctx.host == 'install.elko.io':
+    if ctx.host == 'get.elko.io':
         ctx.response_headers['Content-Type'] = 'text/plain; charset=utf-8'
         return INSTALL_SCRIPT
     return ctx.render_mako_template(
